407dce9e80ecac4a2e69ad9c856075db243975d6
EliteMonsterDrinker <misha.kusnets@yandex.ua>
2026-09-21 01:38:24 +0300

    добавил обработку команд от пользователя, доработал интерфейс, добавил системное приглашение в начало каждой строки(как в терминале)

--- UI.java
+++ UI.java
@@ -1,14 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
+import java.awt.event.*;
 import java.net.InetAddress;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.net.UnknownHostException;
 
 public class UI extends JFrame{
+    private String prompt; //системное приглашение
     private JTextArea outputArea;
     private JTextField inputField;
+
     public UI(){
         //создаём окно на основе реальных данных пк
         String username = System.getProperty("user.name");
@@ -21,11 +23,14 @@ public class UI extends JFrame{
             hostname = "unknown";
         }
 
+        prompt = username + "@" + hostname + ":~$ ";
+
         setTitle("Эмулятор:  [" + username + "@" + hostname + "]");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         //вычисляем размер
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-        setSize(screen.width, screen.height);
+        //такая высота, так как иначе текстовая строка плохо в окно помещается. Выбрал опытным путём
+        setSize(screen.width, screen.height-75);
         //тут мы рисуем сам интерфейс
 
         //поле вывода
@@ -36,6 +41,8 @@ public class UI extends JFrame{
         outputArea.setForeground(Color.ORANGE);
         outputArea.setFont(new Font("Monospaced", Font.PLAIN, 14));
 
+        outputArea.append("Добро пожаловать в терминал! \n" + prompt);
+
         //добавляет пролистывание страницы и рендерит текстовые поля
         JScrollPane scrollPane = new JScrollPane(outputArea);
 
@@ -43,15 +50,32 @@ public class UI extends JFrame{
         inputField = new JTextField();
         inputField.setBackground(Color.BLACK);
         inputField.setForeground(Color.GREEN);
-        inputField.setFont(new Font("Monospaced", Font.PLAIN, 14));
+        inputField.setFont(new Font("Monospaced", Font.PLAIN, 16));
         inputField.setCaretColor(Color.GREEN);
+
+
+        // добавляем обработку ввода от пользователя
+        inputField.addActionListener(new ActionListener(){ //создаём слушателя событий
+            @Override
+            public void actionPerformed(ActionEvent e){ //создаём последовательность реакции в ответ на событие(энтер)
+                String command = inputField.getText().trim();
+                if(!command.isEmpty()){
+                    outputArea.append(command + "\n");
+                    outputArea.append(prompt);
+                    inputField.setText(""); //очищаем текстовое поле ввода
+                    outputArea.setCaretPosition(outputArea.getDocument().getLength()); //опускаем каретку ниже
+                }
+
+            }
+        });
+
         //расставляет элементы в окне
         add(scrollPane, BorderLayout.CENTER);
         add(inputField, BorderLayout.SOUTH);
         setVisible(true);
         }
 
-        public static void main(String[] args){
-            SwingUtilities.invokeLater(()-> new UI());
+        public static void main(String[] args) {
+                UI test = new UI();
         }
 }
