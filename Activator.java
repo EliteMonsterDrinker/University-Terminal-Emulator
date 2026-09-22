19f7a9f6e418f908a272b71134abb62903f4fdd3
EliteMonsterDrinker <misha.kusnets@yandex.ua>
2026-09-22 16:59:45 +0300

    добавил комментарии в main

--- Activator.java
+++ Activator.java
@@ -6,33 +6,33 @@ import javax.swing.SwingUtilities;
 
 public class Activator {
     public static void main(String[] args) {
-        final BlockingQueue<String> queue = new LinkedBlockingQueue<String>();
-        final Path startDir = Path.of(System.getProperty("user.dir"));
+        final BlockingQueue<String> queue = new LinkedBlockingQueue<String>(); //создаёт общий почтовый ящик, куда интерфейс шлёт команды, а интерпретатор парсит их оттуда
+        final Path startDir = Path.of(System.getProperty("user.dir")); //начальная папка
 
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                UI ui = new UI(queue);
+                UI ui = new UI(queue); //создаёт интерфейс
 
                 Consumer<String> output = new Consumer<String>() {
                     @Override
                     public void accept(String s) {
                         ui.appendOutput(s);
                     }
-                };
+                }; // когда кто-то вызывает accept, получает строку и добавляет в текстовое поле
 
                 Runnable prompt = new Runnable() {
                     @Override
                     public void run() {
-                        ui.showPrompt();
+                        ui.showPrompt(); //когда кто-то вызывает run, показывает системное приглашение
                     }
                 };
 
                 CommandInterpreter interp =
-                new CommandInterpreter(queue, output, prompt, startDir);
+                new CommandInterpreter(queue, output, prompt, startDir); //основная логика программы
 
-                Thread worker = new Thread(interp, "terminal-worker");
-                worker.setDaemon(true);
+                Thread worker = new Thread(interp, "terminal-worker"); //создаёт новый поток, в котором будет вся обработка комманд. Не в потоке swing
+                worker.setDaemon(true); //работает в фоне
                 worker.start();
 
                 ui.showPrompt();
