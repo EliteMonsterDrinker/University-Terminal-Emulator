e9448b8dc8c782e9e10cbab74c16092ca2550bec
EliteMonsterDrinker <misha.kusnets@yandex.ua>
2026-09-26 18:55:57 +0300

    создал ветку для первой практики

--- CdCommand.java
+++ CdCommand.java
@@ -11,17 +11,6 @@ public class CdCommand implements Command{
     public String name() {return "cd";}
     public String help() {return "Changes directory(меняет папку)";}
     public void execute(List<String> args, CommandContext ctx) throws Exception{
-      Path target;
-      if(args.isEmpty()){
-        target=Path.of(System.getProperty("user.home"));
-      }
-      else{
-        target = ctx.cwd().resolve(args.get(0)).normalize();
-      }
-      //проверяем, существует ли целевая директория
-      if(!Files.exists(target)){
-        throw new Exception("no such directory: " + target); //если не существует, кидаем исключение
-      }
-      ctx.setCwd(target.toRealPath()); //если существует, вызывает метод setCwd
+      ctx.println("cd");
     }
 }
--- LsCommand.java
+++ LsCommand.java
@@ -8,16 +8,6 @@ public class LsCommand implements Command{
     public String name(){return "ls";}
     public String help(){return "list directory(показывает список файлов внутри директории)";}
     public void execute(List<String> args, CommandContext ctx) throws IOException{
-        //если путь пустой - переводит в домашнюю папку, иначе по аргументу
-        Path dir;
-        if(args.isEmpty()){
-            dir = ctx.cwd();
-        }
-        else{
-            dir = ctx.cwd().resolve(args.get(0));
-        }
-        try(Stream<Path> s = Files.list(dir)){
-            s.sorted().forEach(p -> ctx.println(p.getFileName().toString())); //выводит отсортированный список всех файлов в директории
-        }
+        ctx.println("ls");
     }
 }
